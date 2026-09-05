05c8ffc2c20b9a6704406b9e935fd2c8af411210
Sahas Gawade <sahasmgawade@gmail.com>
2026-09-06 01:22:22 +0530

    Changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,7 +10,7 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
-      <MemoryRouter initialEntries={['/']}>
+      <MemoryRouter initialEntries={[window.location.pathname + window.location.search]}>
         <AuthProvider>
           <StudentAuthProvider>
             <FacultyAuthProvider>
