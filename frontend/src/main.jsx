ceb5831f5134a249c1a0d84c0ecd94609f34ffec
Sahas Gawade <sahasmgawade@gmail.com>
2026-09-20 22:57:39 +0530

    Login Page Changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { MemoryRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import { AuthProvider } from './context/AuthContext.jsx';
 import { StudentAuthProvider } from './context/StudentAuthContext.jsx';
@@ -10,7 +10,7 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
-      <MemoryRouter initialEntries={[window.location.pathname + window.location.search]}>
+      <BrowserRouter>
         <AuthProvider>
           <StudentAuthProvider>
             <FacultyAuthProvider>
@@ -18,7 +18,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
             </FacultyAuthProvider>
           </StudentAuthProvider>
         </AuthProvider>
-      </MemoryRouter>
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
 );
